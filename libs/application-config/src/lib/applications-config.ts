1649ebc55d6910ce1474bbb7928b3e482edbddc3
zhumeisongsong <zhumeisongsong@gmail.com>
2024-11-07 10:06:57 +0900

    feat: ✨ add DEFAULT_HOST

--- libs/application-config/src/lib/applications-config.ts
+++ libs/application-config/src/lib/applications-config.ts
@@ -1,22 +1,24 @@
+const DEFAULT_HOST = 'localhost';
+
 const DEFAULT_PORT = {
-  user: 15001,
-  task: 15002,
-  gateway: 3333,
+  user: '15001',
+  task: '15002',
+  gateway: '3333',
 };
 
 // Gateway
 export const gatewayConfig = {
-  host: process.env['GATEWAY_HOST'] ?? undefined,
+  host: process.env['GATEWAY_HOST'] ?? DEFAULT_HOST,
   port: process.env['GATEWAY_PORT'] ?? DEFAULT_PORT.gateway,
 };
 
 // Graphql
 export const userSubGraph = {
-  host: process.env['USER_HOST'] ?? undefined,
+  host: process.env['USER_HOST'] ?? DEFAULT_HOST,
   port: process.env['USER_PORT'] ?? DEFAULT_PORT.user,
 };
 
 export const taskSubGraph = {
-  host: process.env['TASK_HOST'] ?? undefined,
+  host: process.env['TASK_HOST'] ?? DEFAULT_HOST,
   port: process.env['TASK_PORT'] ?? DEFAULT_PORT.task,
 };
