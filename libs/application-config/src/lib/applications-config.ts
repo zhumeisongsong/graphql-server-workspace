3ba5a8ec193ea788e8cf62172af149299663140a
zhumeisongsong <zhumeisongsong@gmail.com>
2024-11-07 10:59:40 +0900

    refactor: ♻️ rename ServiceConfig to ApplicationConfig

--- libs/application-config/src/lib/applications-config.ts
+++ libs/application-config/src/lib/applications-config.ts
@@ -1,4 +1,4 @@
-interface ServiceConfig {
+interface ApplicationConfig {
   host: string;
   port: string;
 }
@@ -12,18 +12,18 @@ const DEFAULT_PORT = {
 };
 
 // Gateway
-export const gatewayConfig: ServiceConfig = {
+export const gatewayConfig: ApplicationConfig = {
   host: process.env['GATEWAY_HOST'] ?? DEFAULT_HOST,
   port: process.env['GATEWAY_PORT'] ?? DEFAULT_PORT.gateway,
 };
 
 // Graphql
-export const userSubGraph: ServiceConfig = {
+export const userSubGraph: ApplicationConfig = {
   host: process.env['USER_HOST'] ?? DEFAULT_HOST,
   port: process.env['USER_PORT'] ?? DEFAULT_PORT.user,
 };
 
-export const taskSubGraph: ServiceConfig = {
+export const taskSubGraph: ApplicationConfig = {
   host: process.env['TASK_HOST'] ?? DEFAULT_HOST,
   port: process.env['TASK_PORT'] ?? DEFAULT_PORT.task,
 };
