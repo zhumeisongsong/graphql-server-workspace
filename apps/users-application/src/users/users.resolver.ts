e1697fa0d403a27ffc9b06ae343e16a62a163279
zhumeisongsong <zhumeisongsong@gmail.com>
2024-11-08 11:27:37 +0900

    feat: ✨ add user resolver

---
+++ apps/users-application/src/users/users.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, ID, Query, Resolver, ResolveReference } from '@nestjs/graphql';
+
+import { User } from './models/user.model';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private usersService: UsersService) {}
+
+  @Query()
+  getUser(@Args({ name: 'id', type: () => ID }) id: number): User | undefined {
+    return this.usersService.findById(id);
+  }
+
+  @ResolveReference()
+  resolveReference(reference: {
+    __typename: string;
+    id: number;
+  }): User | undefined {
+    return this.usersService.findById(reference.id);
+  }
+}
