859bbeddb86297a7db273070a6b4891c0f52a6ab
zhumeisongsong <zhumeisongsong@gmail.com>
2024-11-16 00:09:17 +0900

    feat: ✨ add user-infrastructure-graphql

---
+++ libs/user/infrastructure/grapgql/README.md
@@ -0,0 +1,7 @@
+# user-infrastructure-graphql
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test user-infrastructure-graphql` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/user/infrastructure/grapgql/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'user-infrastructure-graphql',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/user/infrastructure/graphql',
+};
