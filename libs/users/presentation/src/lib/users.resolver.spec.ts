2787002a0d042f242a4053d6d88c2553adfe3307
zhumeisongsong <zhumeisongsong@gmail.com>
2024-11-18 11:48:59 +0900

    feat: ✨ Move users.resolver to users/presentation

---
+++ libs/users/presentation/src/lib/users.resolver.spec.ts
@@ -0,0 +1,68 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from '../../../application/src/lib/users.service';
+import { User } from '@user/domain';
+
+describe('UsersResolver', () => {
+  let resolver: UsersResolver;
+  let service: UsersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: {
+            findById: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    resolver = module.get<UsersResolver>(UsersResolver);
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should return a user by id', () => {
+      const user: User = { id: '1', name: 'John Doe' };
+      jest.spyOn(service, 'findById').mockReturnValue(user);
+
+      expect(resolver.getUser('1')).toEqual(user);
+      expect(service.findById).toHaveBeenCalledWith('1');
+    });
+
+    it('should return undefined if user not found', () => {
+      jest.spyOn(service, 'findById').mockReturnValue(undefined);
+
+      expect(resolver.getUser('2')).toBeUndefined();
+      expect(service.findById).toHaveBeenCalledWith('2');
+    });
+  });
+
+  describe('resolveReference', () => {
+    it('should return a user by reference id', () => {
+      const user: User = { id: '1', name: 'John Doe' };
+      jest.spyOn(service, 'findById').mockReturnValue(user);
+
+      expect(
+        resolver.resolveReference({ __typename: 'User', id: '1' }),
+      ).toEqual(user);
+      expect(service.findById).toHaveBeenCalledWith('1');
+    });
+
+    it('should return undefined if user not found by reference id', () => {
+      jest.spyOn(service, 'findById').mockReturnValue(undefined);
+
+      expect(
+        resolver.resolveReference({ __typename: 'User', id: '2' }),
+      ).toBeUndefined();
+      expect(service.findById).toHaveBeenCalledWith('2');
+    });
+  });
+});
---
+++ libs/users/presentation/src/lib/users.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, ID, Query, Resolver, ResolveReference } from '@nestjs/graphql';
+import { User } from '@user/domain';
+
+import { UsersService } from '../../../application/src/lib/users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private usersService: UsersService) {}
+
+  @Query(() => User, { nullable: true })
+  getUser(@Args({ name: 'id', type: () => ID }) id: string): User | undefined {
+    return this.usersService.findById(id);
+  }
+
+  @ResolveReference()
+  resolveReference(reference: {
+    __typename: string;
+    id: string;
+  }): User | undefined {
+    return this.usersService.findById(reference.id);
+  }
+}
