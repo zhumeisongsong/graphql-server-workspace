624c562ba2fa6d2fcaae898f7b64f4f3655c9063
zhumeisongsong <zhumeisongsong@gmail.com>
2024-11-27 15:24:13 +0900

    docs: 📝 Fix inconsistency between comment and implementation

--- libs/users/domain/src/lib/user.entity.ts
+++ libs/users/domain/src/lib/user.entity.ts
@@ -1,5 +1,4 @@
 // Ensure that the values of all attributes entering the domain layer are valid.
-// The constructor is private to prevent direct instantiation of the class.
 
 export class User {
   // Class attributes can be declared and initialized directly in the constructor arguments
