07b0d476b372d9d4940293ad4e6a050bbf599c05
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-02 16:37:49 +0900

    fix: 🐛 Add required dependencies to the auth module

--- libs/auth/interface-adapters/src/lib/auth.module.ts
+++ libs/auth/interface-adapters/src/lib/auth.module.ts
@@ -1,9 +1,10 @@
+import { AuthService } from '@auth/application';
 import { Module } from '@nestjs/common';
 
 import { AuthResolver } from './resolver/auth.resolver';
 
 @Module({
-  providers: [AuthResolver],
+  providers: [AuthResolver, AuthService],
   exports: [],
 })
 export class AuthModule {}
