edb09bbad1d948345720d488aea17e15152cbc9e
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-03 20:06:28 +0900

    fix: 🐛 AuthModule is not valid

--- libs/auth/interface-adapters/src/lib/auth.module.ts
+++ libs/auth/interface-adapters/src/lib/auth.module.ts
@@ -1,10 +1,21 @@
 import { AuthService } from '@auth/application';
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AwsCognitoService } from '@shared/infrastructure-aws-cognito';
+import { UsersService } from '@users/application';
+import { UsersModule } from '@users/interface-adapters';
 
 import { AuthResolver } from './resolver/auth.resolver';
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  providers: [
+    AuthResolver,
+    AuthService,
+    AwsCognitoService,
+    UsersService,
+    JwtService,
+  ],
+  imports: [UsersModule, JwtModule],
   exports: [],
 })
 export class AuthModule {}
