28194fd732298f2d86886560987af61b4a77ab11
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-03 18:52:31 +0900

    feat: ✨ register JwtModule as global

--- libs/auth/interface-adapters/src/lib/auth.module.ts
+++ libs/auth/interface-adapters/src/lib/auth.module.ts
@@ -1,10 +1,27 @@
 import { AuthService } from '@auth/application';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '@users/interface-adapters';
 
 import { AuthResolver } from './resolver/auth.resolver';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: (configService: ConfigService) => {
+        const jwtConfig = configService.get('jwt');
+        return {
+          secret: jwtConfig.secret,
+          signOptions: { expiresIn: '60s' },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthResolver, AuthService],
-  exports: [],
+  exports: [AuthService],
 })
 export class AuthModule {}
