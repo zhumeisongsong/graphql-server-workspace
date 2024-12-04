52733d54dd4960b69151ea09f10ac35168245e72
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-04 12:29:03 +0900

    refactor: ♻️ move ApolloFederationDriverConfig to app module

--- apps/users/src/app/app.module.ts
+++ apps/users/src/app/app.module.ts
@@ -1,5 +1,11 @@
+import { ApolloServerPluginInlineTrace } from '@apollo/server/plugin/inlineTrace';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import {
   databaseConfig,
   userAppConfig,
@@ -17,6 +23,20 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [userAppConfig, databaseConfig, awsConfig, authConfig],
+    }),
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        /**
+         * MEMO:
+         * Because of this problem, so mush need specify the version
+         * https://github.com/nestjs/graphql/issues/2646#issuecomment-1567381944
+         */
+        federation: 2,
+      },
+      playground: process.env['NODE_ENV'] !== 'production',
+      sortSchema: true,
+      plugins: [ApolloServerPluginInlineTrace()],
     }),
     UsersModule,
     AuthModule,
