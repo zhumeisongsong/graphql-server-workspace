c6bf72b37a378e8540de42a31d6e8cff90536843
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-05 22:59:06 +0900

    fix: 🐛 can not return access token

--- libs/auth/interface-adapters/src/lib/auth.module.ts
+++ libs/auth/interface-adapters/src/lib/auth.module.ts
@@ -1,39 +1,15 @@
 import { AuthService } from '@auth/application';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { AwsCognitoService } from '@shared/infrastructure-aws-cognito';
-import { DatabaseModule } from '@shared/infrastructure-mongoose';
-import { GetUserByEmailUseCase, GetUserByIdUseCase, UsersService } from '@users/application';
-import { USERS_REPOSITORY } from '@users/domain';
-import {
-  MongooseUsersRepository,
-  UserDocument,
-  UserSchema,
-} from '@users/infrastructure-mongoose';
-import { UsersModule } from '@users/interface-adapters';
 
 import { AuthResolver } from './resolver/auth.resolver';
 
 @Module({
-  providers: [
-    AuthResolver,
-    AuthService,
-    AwsCognitoService,
-    UsersService,
-    JwtService,
-    GetUserByIdUseCase,
-    GetUserByEmailUseCase,
-    {
-      provide: USERS_REPOSITORY,
-      useClass: MongooseUsersRepository,
-    },
-  ],
+  providers: [AuthResolver, AuthService, AwsCognitoService],
   imports: [
-    UsersModule,
     JwtModule.registerAsync({
-      global: true,
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
@@ -45,10 +21,6 @@ import { AuthResolver } from './resolver/auth.resolver';
         };
       },
     }),
-    DatabaseModule,
-    MongooseModule.forFeature([
-      { name: UserDocument.name, schema: UserSchema },
-    ]),
   ],
   exports: [AuthService],
 })
