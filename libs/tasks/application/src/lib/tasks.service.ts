a40db58045f985426d1e113a6d95370f08f7fb5e
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-07 22:33:42 +0900

    fix: 🐛 delete empty constructor

--- libs/tasks/application/src/lib/tasks.service.ts
+++ libs/tasks/application/src/lib/tasks.service.ts
@@ -3,8 +3,6 @@ import { Task, UserTask } from '@tasks/domain';
 
 @Injectable()
 export class TasksService {
-  constructor() {}
-
   async findAll(): Promise<Task[]> {
     // TODO: Implement this
     return [];
