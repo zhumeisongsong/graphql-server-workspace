48646b7e1fe0b999226bf5c0d46efa39e89b54c1
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-07 22:03:43 +0900

    fix: 🐛 fix TaskRepository definition

--- libs/tasks/domain/src/lib/task.repository.ts
+++ libs/tasks/domain/src/lib/task.repository.ts
@@ -3,7 +3,16 @@ import { UserTask } from './entities/user-task.entity';
 
 export interface TaskRepository {
   findAllTasks(): Promise<Task[]>;
-  findUserTasks(userId: string): Promise<Task[]>;
-  createUserTasks(userId: string, tasks: Task[]): Promise<void>;
-  updateUserTasks(userId: string, userTasks: UserTask[]): Promise<void>;
+  findUserTasks(
+    userId: string,
+    range?: { from: Date; to: Date },
+  ): Promise<Task[]>;
+  createUserTasks(
+    userId: string,
+    tasks: { id: string; createdAt: Date }[],
+  ): Promise<void>;
+  updateUserTasks(
+    userId: string,
+    userTasks: { id: string; updatedAt: Date }[],
+  ): Promise<void>;
 }
