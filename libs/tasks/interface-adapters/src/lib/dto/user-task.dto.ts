d2ba8cb1d853d51da5d650f937b15607520cf589
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-09 10:06:30 +0900

    feat: ✨ Add validation decorators to UserTaskDto

--- libs/tasks/interface-adapters/src/lib/dto/user-task.dto.ts
+++ libs/tasks/interface-adapters/src/lib/dto/user-task.dto.ts
@@ -1,29 +1,42 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { UserDto } from '@users/interface-adapters';
+import { IsDate, IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 
 import { TaskDto } from './task.dto';
 
 @ObjectType()
 export class UserTaskDto {
   @Field(() => ID)
+  @IsNotEmpty()
+  @IsUUID()
   id: string;
 
   @Field(() => Date)
+  @IsNotEmpty()
+  @IsDate()
   createdAt: Date;
 
   @Field(() => Date, { nullable: true })
+  @IsOptional()
+  @IsDate()
   updatedAt: Date | null;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsUUID()
   taskId: string;
 
   @Field(() => TaskDto, { nullable: true })
+  @IsOptional()
   task: TaskDto | null;
 
   @Field(() => String)
+  @IsNotEmpty()
+  @IsUUID()
   userId: string;
 
   @Field(() => UserDto, { nullable: true })
+  @IsOptional()
   user: UserDto | null;
 
   constructor(
