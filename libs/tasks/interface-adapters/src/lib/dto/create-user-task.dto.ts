7ee395926f59ec69fba7fd514ff5b4cd96a5bdb2
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-09 09:49:21 +0900

    feat: ✨ Add validation for the id field

--- libs/tasks/interface-adapters/src/lib/dto/create-user-task.dto.ts
+++ libs/tasks/interface-adapters/src/lib/dto/create-user-task.dto.ts
@@ -1,8 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsUUID } from 'class-validator';
 
 @InputType()
 export class CreateUserTaskDto {
   @Field(() => String)
+  @IsNotEmpty()
+  @IsUUID()
   id: string;
 
   @Field(() => Date)
