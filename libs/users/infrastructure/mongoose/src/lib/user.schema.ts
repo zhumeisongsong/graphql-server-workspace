a5dd726c2197027e42bb13b821b574fa11c5ba67
zhumeisongsong <zhumeisongsong@gmail.com>
2024-12-13 22:28:55 +0900

    feat: ✨ add maxlength and default to UserDocument

--- libs/users/infrastructure/mongoose/src/lib/user.schema.ts
+++ libs/users/infrastructure/mongoose/src/lib/user.schema.ts
@@ -4,12 +4,18 @@ import { Document } from 'mongoose';
 @Schema()
 export class UserDocument extends Document {
   // refactor: move match regex to a shared lib
-  @Prop({ required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })
+  @Prop({
+    required: true,
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    maxlength: 100,
+  })
   email!: string;
 
-  @Prop({ required: false})
+  @Prop({ required: false, default: null, maxlength: 100 })
   firstName!: string;
-  @Prop({ required: false })
+
+  @Prop({ required: false, default: null, maxlength: 100 })
   lastName!: string;
 }
 
