26b33eb33858afd1d2a25eda166b2ff93fb96506
zhumeisongsong <zhumeisongsong@gmail.com>
2025-01-27 18:47:19 +0900

    fix: 🐛 lint error

--- libs/self-care-tasks/domain/src/lib/entities/self-care-user-task.entity.ts
+++ libs/self-care-tasks/domain/src/lib/entities/self-care-user-task.entity.ts
@@ -1,16 +1,15 @@
-import { TaskStatus } from '@tasks/domain';
-import { User } from '@users/domain';
+// import { TaskStatus } from '@tasks/domain';
+// import { User } from '@users/domain';
 
 import { SelfCareTask } from './self-care-task.entity';
-import { SelfCareTopic } from './self-care-topic.entity';
 
 export interface SelfCareUserTask {
   id: string;
   taskId: string;
   task: SelfCareTask;
   userId: string;
-  user: User;
-  status: TaskStatus;
+  // user: User;
+  // status: TaskStatus;
   createdAt: string;
   scheduledAt: string;
   updatedAt: string;
