592124595cc62bfc91a073facec6eab0de0759ca
zhumeisongsong <zhumeisongsong@gmail.com>
2025-02-02 14:15:29 +0900

    feat: ✨ remove serve error gateway

---
+++ apps/self-care-gateway/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'self-care-gateway',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/self-care-gateway',
+};
