99e23b6fcd75958e11bcc7342a4f7495e6af8557
zhumeisongsong <zhumeisongsong@gmail.com>
2025-02-02 17:05:16 +0900

    feat: ✨ remove AwsCognitoModule

--- libs/shared/infrastructure/aws-cognito/src/lib/aws-cognito.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AwsCognitoService } from './aws-cognito.service';
-
-@Module({
-  providers: [AwsCognitoService],
-  exports: [AwsCognitoService],
-})
-export class AwsCognitoModule {}
