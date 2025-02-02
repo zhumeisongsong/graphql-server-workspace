9fac76324e620890b161486a765b3e3a405a0eab
zhumeisongsong <zhumeisongsong@gmail.com>
2025-02-02 22:52:23 +0900

    revert: ⏪️ move apps config back to @shared/config

--- libs/shared/config/src/index.ts
+++ libs/shared/config/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/database.config';
 export * from './lib/aws.config';
 export * from './lib/auth.config';
 export * from './lib/deep-seek.config';
+export * from './lib/apps.config';
