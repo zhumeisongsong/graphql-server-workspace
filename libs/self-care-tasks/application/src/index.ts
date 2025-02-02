d25ed3c89fbf0d398eec03db3e321914ab985ca3
zhumeisongsong <zhumeisongsong@gmail.com>
2025-02-02 16:43:15 +0900

    feat: ✨ Inject AI_SERVICE into SelfCareTasksService

--- libs/self-care-tasks/application/src/index.ts
+++ libs/self-care-tasks/application/src/index.ts
@@ -1,1 +1,3 @@
+export * from './lib/self-care-tasks.service';
+
 export * from './lib/use-cases/generate-some-self-care-tasks.use-case';
--- libs/self-care-tasks/application/src/lib/self-care-tasks.service.ts
+++ libs/self-care-tasks/application/src/lib/self-care-tasks.service.ts
@@ -1,11 +1,21 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { SelfCareTask, SelfCareTopic, SelfCareUserTask } from '@self-care-tasks/domain';
+import { AI_SERVICE, AIService } from '@ai/domain';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import {
+  SelfCareTask,
+  SelfCareTopic,
+  SelfCareUserTask,
+} from '@self-care-tasks/domain';
 
 @Injectable()
 export class SelfCareTasksService {
-  constructor() {
-    //TODO:  add ai service here
-  }
+  constructor(
+    @Inject(AI_SERVICE)
+    private aiService: AIService,
+  ) {}
   async findMany(userId: string): Promise<SelfCareUserTask[]> {
     // TODO: Implement this
     return [];
@@ -15,7 +25,17 @@ export class SelfCareTasksService {
     selfCareTopics: SelfCareTopic[],
     count: number,
   ): Promise<SelfCareTask[]> {
-    const selfCareTasks: SelfCareTask[] = [];
+    const prompt = this.buildPrompt(selfCareTopics, count);
+
+    const response = await this.aiService.chat({
+      messages: [{ role: 'user', content: prompt, timestamp: new Date() }],
+      temperature: 0.7,
+      json: true,
+    });
+
+    const selfCareTasks = response.content
+      .split('\n')
+      .map((task) => task.trim());
 
     if (selfCareTasks.length === 0) {
       throw new InternalServerErrorException(
@@ -23,6 +43,21 @@ export class SelfCareTasksService {
       );
     }
 
-    return selfCareTasks;
+    return [];
+  }
+
+  private buildPrompt(selfCareTopics: SelfCareTopic[], count: number): string {
+    return `
+    You are a mental health assistant. Please generate self care tasks based on the following conditions:
+    1.topics: ${selfCareTopics.map((topic) => topic.name).join(', ')}
+    2.task count: ${count}
+
+    requirements:
+    - response should be in English
+    - response format: json
+    - response should be an array of objects, each object should have the following properties:
+      - name: string, should be a short name
+      - description: string, should be a short description
+    `;
   }
 }
