1909097d755f23663459c0358c30a2e0ecd7d44f
zhumeisongsong <zhumeisongsong@gmail.com>
2025-03-17 23:50:38 +0900

    refactor: ♻️ rename AppModule to SelfCareHubAppModule

--- apps/self-care-hub/src/main.ts
+++ apps/self-care-hub/src/main.ts
@@ -5,10 +5,10 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { SelfCareHubAppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(SelfCareHubAppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
